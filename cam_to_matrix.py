63da3d1a770331d9acb7abccf3a48ca057d5d892
crocdialer <crocdialer@googlemail.com>
2015-05-30 16:58:53 +0000

    led matrix working, wrong adressing though

--- cam_to_matrix.py
+++ cam_to_matrix.py
@@ -6,21 +6,49 @@ raspberry pi camera to ht1632-c utility
 author: crocdialer@gmail.com
 '''
 
-import ht1632c, cv2, picamera, time
+import ht1632c, cv2, picamera, time, random
+
+WIDTH = 24
+HEIGHT = 16
+COLOR = 1
 
 class App(object):
   def __init__(self):
     self.led_matrix = ht1632c.HT1632C(1, False)
-    self.cam = picamera.PiCamera()
+    #self.cam = picamera.PiCamera()
+    self.running = False
 
   def run(self):
-    while True:
+
+    self.running = True
+    while self.running:
     
       try:
-        pass
+        self.led_matrix.clear()
+
+        #for x in range(0, WIDTH):
+        #  for y in range(0, HEIGHT):
+        #    self.led_matrix.plot(x, y, 1)
+        #    time.sleep(.33)
+        #    self.led_matrix.sendframe()
+       
+        #for x in range(0, WIDTH):
+        #  for y in range(0, HEIGHT):
+        #    self.led_matrix.plot(x, y, 0)
+        #    time.sleep(.33)
+        #    self.led_matrix.sendframe()
+        
+        sz = (random.randint(0, WIDTH), random.randint(0, HEIGHT))
+        self.led_matrix.box(0, 0, sz[0], sz[1], 1)
+        
+        self.led_matrix.sendframe()
+        #print("sendframe")
+        time.sleep(.15)
 
       except KeyboardInterrupt:
         print("\nkeyboard interrupt")
+        self.led_matrix.close()
+        self.running = False
 
 ##########################################################################
 
